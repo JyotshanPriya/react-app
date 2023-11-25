0a56cbfde9068e40019407b04de205316df39b28
JyotshanPriya <141807883+JyotshanPriya@users.noreply.github.com>
2023-11-25 17:05:54 +0530

    h3 added

--- App1.js
+++ App1.js
@@ -27,7 +27,8 @@ const parent = React.createElement(
       "div",
       {id:'child2'},
       [React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag")]
+      React.createElement("h2", {}, "I'm an h2 tag"),
+      React.createElement("h3", {}, "I'm an h2 tag")]
     )
   ]
 );
